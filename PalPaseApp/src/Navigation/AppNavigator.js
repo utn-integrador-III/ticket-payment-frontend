9b73cf9bbe8ed92b7767b96fb84db3b9ce3a28e6
David <daruizva@est.utn.ac.cr>
2025-07-03 21:01:35 -0600

    Creacion UserProfileScreen

--- PalPaseApp/src/Navigation/AppNavigator.js
+++ PalPaseApp/src/Navigation/AppNavigator.js
@@ -3,18 +3,20 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "../Screens/LoginScreen";
 import RegisterScreen from "../Screens/RegisterScreen";
 import HomeScreen from "../Screens/HomeScreen";
+import UserScreen from "../Screens/UserScreen"; // Asegúrate de que el nombre del archivo sea correcto
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   return (
     <Stack.Navigator
-      initialRouteName="Login"
+      initialRouteName="User"
       screenOptions={{ headerShown: false }}
     >
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="User" component={UserScreen} />
     </Stack.Navigator>
   );
 }
