a95b156ed37192701febb0c7ebb9d3dfac4af782
msanchezb18 <msanchezb@est.utn.ac.cr>
2025-07-31 19:46:10 -0600

    US-MostrarQR: Creación de la Pantalla del QR

---
+++ PalPaseApp/src/Screens/QRCodeScreen.js
@@ -0,0 +1,86 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import QRCode from "react-native-qrcode-svg";
+
+export default function QRCodeScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <ImageBackground
+      source={require("../../assets/fondo.png")}
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <View style={styles.container}>
+        {/* Logo */}
+        <Image
+          source={require("../../assets/logo-palpase.png")}
+          style={styles.logo}
+        />
+
+        {/* Código QR */}
+        <QRCode value="https://palpase.com/usuario123" size={200} />
+
+        {/* Botón Regresar */}
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => navigation.goBack()}
+        >
+          <Text style={styles.backButtonText}>Regresar</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 40,
+  },
+  iconTopLeft: {
+    position: "absolute",
+    top: 40,
+    left: 20,
+  },
+  iconTopRight: {
+    position: "absolute",
+    top: 40,
+    right: 20,
+  },
+  iconImage: {
+    width: 30,
+    height: 30,
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 30,
+    resizeMode: "contain",
+  },
+  backButton: {
+    backgroundColor: "#a5d2cf",
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderRadius: 20,
+    marginTop: 30,
+  },
+  backButtonText: {
+    color: "#000",
+    fontSize: 16,
+  },
+});
